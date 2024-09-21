3f884b17aacd27d589c9e29d665afdfeb4d075b2
Oh-Kang94 <okh19941994@naver.com>
2024-09-21 20:23:25 +0900

    🔧 Ready and Set Conf for delpoy

--- 2. smart_contract_practice/fund_raising.sol
+++ 2. smart_contract_practice/fund_raising.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 // 컴파일러 버젼을 잠궈야할 필요성이 있기 때문에
 pragma solidity ^0.8.0;
 
@@ -16,7 +17,7 @@ contract FundRaising {
     // 생성자에서 아무것도 정의하지 않았지만, EVM안에서 코드가 실행될때 정의될것이다.
     // ## Constructor
     constructor(uint256 _targetAmount) {
-        targetAmount = _targetAmount;
+        targetAmount = uint64(_targetAmount);
         owner = msg.sender;
         // msg는 블록 글로벌 변수이다.
     }
@@ -25,7 +26,7 @@ contract FundRaising {
     // `external`은 외부에서만 실행될 수 있다는 것을 알림
     // `payable`은 이 함수가 돈을 받을 수 있다는 것을 알림
     // `require`은 조건문을 거는데, 만일 False이면, ','의 뒤의 메시지를 Throw시킬거임
-    function receive() external payable {
+    receive() external payable { // 이부분에서 function을 빼는 이유는 이더가 컨트랙트로 직접 전송될 때 이 함수가 호출되는 특수 함수이기 때문에
         require(block.timestamp < finishTime, "This campaign is over"); // 여기서 block.timestamp란 외부에서 보낸 날짜를 의미
         donations[msg.sender] += msg.value; // Hash에 저장 `+=`을 하는 이유는 추가로 보낼수 있기 때문에
         raisedAmount += msg.value;
---
+++ hardhat.config.js
@@ -0,0 +1,8 @@
+/** 
+ * @type import('hardhat/config').HardhatUserConfig 
+*/
+require("@nomiclabs/hardhat-waffle")
+module.exports = {
+  solidity: "0.8.0",
+};
+// .sol 과 solidity 버젼을 맞춰야함
